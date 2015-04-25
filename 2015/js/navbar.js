e318d0fd923bfa528525a65ae2ded2a03b3f2b12
Tony Yip <tony@opensource.hk>
2015-04-25 21:35:42 +0800

    Merge main.js and navbar.js in 2015

--- 2015/js/navbar.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-    var url = document.URL.replace(window.location.protocol + '//').split('/')[2];
-    if ('' === url)
-        url = 'brand';
-    $('.navbar a#' + url).addClass('nav-highlight');
-}(window.jQuery));
